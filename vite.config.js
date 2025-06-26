bcaa158e298ff9d6b23ddb819da36730fa7f1adf
Ahmed Marnissi <109542834+Ahmed-Marnissi@users.noreply.github.com>
2025-06-26 23:53:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // or '/Ahmed-Marnissi.github.io/' if needed
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
